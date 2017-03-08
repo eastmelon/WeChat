68d3e38f63876a12cccfa290a450fa1cdc328806
eastmelon <eastmelon_1@163.com>
2017-03-08 09:41:06 +0800

    remove modules
    remove modules

--- router/main.js
+++ router/main.js
@@ -3,7 +3,7 @@
  */
 var express=require('express');
 var router=express.Router();
-router.get('/chat',function(req,res,next){
+router.get('/',function(req,res,next){
     res.render('index');
 })
 module.exports=router;
